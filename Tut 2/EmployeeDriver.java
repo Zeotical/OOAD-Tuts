047840487cea2bd7ee0d1aba3f2764b2c405c41a
Zeotical <aceyems10@gmail.com>
2025-12-03 19:20:50 +0800

    EmployeeDriver (tut 2)

---
+++ Tut 2/EmployeeDriver.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Lenovo
+ */
+public class EmployeeDriver {
+    public static void main(String[] args){
+    Employee e1 = new Employee("Shah", "Male");
+    Employee e2 = new Employee(6732647, "Ahmad", "Male");
+    System.out.println(e1);
+    System.out.println(e2);
+
+    }
+}
