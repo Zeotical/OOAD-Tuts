58a58f126b0b956d86d3eb176b0ab200ff436fbb
Zeotical <aceyems10@gmail.com>
2025-12-04 18:50:12 +0800

    Update Student.java 3rd step (incomplete) (tut 2)

--- Tut 2/Student.java
+++ Tut 2/Student.java
@@ -6,4 +6,13 @@ public class Student extends Person {
   }
 
   ArrayList<Subject> sub = new ArrayList<Subject> ;
+  int choice;
+  while (choice!=q) {
+  System.out.println("Choose an option to continue: ");
+  System.out.println("[1] Enter new subject" "[2] Remove Subject ") ; //use format l8tr
+  System.out.println("[3] Show results " "[4] Quit ") ;
+  // take user input create scanner earliar(?)
+  
+  
+  }
 }
