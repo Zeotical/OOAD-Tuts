4d20368498eccee822f8721fc48540a67bf2174e
Zeotical <aceyems10@gmail.com>
2025-12-08 16:15:43 +0800

    Student (tut 2)

--- Tut 2/Student.java
+++ Tut 2/Student.java
@@ -1,18 +1,46 @@
+import java.util.*; 
+
+
 public class Student extends Person {
+
+  ArrayList<Subject> sub = new ArrayList<Subject> () ;
 
   public Student(String name, String gender){
 
     super(name,gender);
   }
-
-  ArrayList<Subject> sub = new ArrayList<Subject> ;
-  int choice;
-  while (choice!=q) {
-  System.out.println("Choose an option to continue: ");
-  System.out.println("[1] Enter new subject" "[2] Remove Subject ") ; //use format l8tr
-  System.out.println("[3] Show results " "[4] Quit ") ;
-  // take user input create scanner earliar(?)
   
+  public void addSubject(String subCode, char grade){
+              
+      sub.add(new Subject(subCode,grade)); 
+      
+  }
+  
+  public void remSubject(String subCode){
+    Subject removeSub = null ; // og     Subject removeSub = new Subject("blah",'A'); but can just create null
+       for(Subject i : sub){
+           if(i.getCode().equals(subCode)){
+               
+               removeSub = i;
+            }
+        }
+      sub.remove(removeSub);
+      System.out.println("Subject " + subCode + " removed.");
   
   }
+  
+   
+   public String toString(){ //overiding 
+       return "Student name : " + getName();
+    }
+
+ public void printTranscript(){
+      
+      toString();
+      for(Subject i : sub){
+            System.out.println(i);
+        }
+
+  }
+
 }
